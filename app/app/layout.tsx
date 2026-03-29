fa2d7115986b8cdd78faf78c1e94ad3b214bdd50
nekruza <nekruz.avgani@gmail.com>
2026-03-29 20:35:13 +0100

    feat: add hidden audio element for Inworld Realtime playback

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -44,6 +44,8 @@ export default async function AppLayout({
         <div className="max-w-6xl mx-auto px-4 sm:px-6 pt-6 sm:pt-8 pb-24 md:pb-8">{children}</div>
       </main>
       <Toaster position="top-right" />
+      {/* Hidden audio element for Inworld Realtime agent voice playback */}
+      <audio id="inworld-agent-audio" autoPlay playsInline style={{ display: "none" }} />
     </div>
   );
 }
